a6bba679ad407b2e1f6b07ce82cb25abff3c19e9
Jerahmeel Cosinas <jcosinas@liberty.com.au>
2020-08-16 21:58:16 +1000

    switch to bevy

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,46 @@
-use amethyst::{
-  prelude::*,
-  renderer::{
-      plugins::{RenderShaded3D, RenderSkybox, RenderToWindow},
-      types::DefaultBackend,
-      RenderingBundle,
-  },
-  utils::{application_root_dir},
-};
+use bevy::prelude::*;
 
-mod states;
+fn main() {
+  App::build()
+    .add_resource(Msaa { samples: 4 })
+    .add_default_plugins()
+    .add_startup_system(setup.system())
+    .run();
+}
 
-fn main() -> amethyst::Result<()> {
-
-  amethyst::start_logger(Default::default());
-
-  let app_root = application_root_dir()?;
-  let config_dir = app_root.join("config");
-  let assets_dir = app_root.join("assets");
-
-  let display_config_path = config_dir.join("display.ron");
-
-  let game_data = GameDataBuilder::default()
-      .with_bundle(
-          RenderingBundle::<DefaultBackend>::new()
-              .with_plugin(
-                  RenderToWindow::from_config_path(display_config_path)?
-                      .with_clear([0.0, 0.0, 0.0, 1.0]),
-              )
-              .with_plugin(RenderShaded3D::default())
-              .with_plugin(RenderSkybox::default()),
-      )?;
-
-  let mut game = Application::build(assets_dir, states::GameplayState::default() )?
-      .build(game_data)?;
-
-  game.run();
-  Ok(())
+/// set up a simple 3D scene
+fn setup(
+  mut commands: Commands,
+  mut meshes: ResMut<Assets<Mesh>>,
+  mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+  // add entities to the world
+  commands
+      // plane
+      .spawn(PbrComponents {
+          mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+          material: materials.add(Color::rgb(0.1, 0.2, 0.1).into()),
+          ..Default::default()
+      })
+      // cube
+      .spawn(PbrComponents {
+          mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+          material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
+          translation: Translation::new(0.0, 1.0, 0.0),
+          ..Default::default()
+      })
+      // light
+      .spawn(LightComponents {
+          translation: Translation::new(4.0, 8.0, 4.0),
+          ..Default::default()
+      })
+      // camera
+      .spawn(Camera3dComponents {
+          transform: Transform::new_sync_disabled(Mat4::face_toward(
+              Vec3::new(-3.0, 5.0, 8.0),
+              Vec3::new(0.0, 0.0, 0.0),
+              Vec3::new(0.0, 1.0, 0.0),
+          )),
+          ..Default::default()
+      });
 }
